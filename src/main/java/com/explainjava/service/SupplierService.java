a60dc8be6635dbc4b8629e673e65c18a80b8541b
DawnBreaker <tunganhngo207@gmail.com>
2025-02-19 23:18:55 +0700

    feat: update supplier service

--- src/main/java/com/explainjava/service/SupplierService.java
+++ src/main/java/com/explainjava/service/SupplierService.java
@@ -1,5 +1,36 @@
 package main.java.com.explainjava.service;
 
-public class SupplierService {
+import main.java.com.explainjava.domain.Supplier;
+import main.java.com.explainjava.repository.SupplierRepository;
 
+public class SupplierService {
+    private SupplierRepository supplierRepository;
+
+    public SupplierService(SupplierRepository supplierRepository) {
+	this.supplierRepository = supplierRepository;
+    }
+
+    public Supplier saveSupplier(int id, String name, String contactEmail) {
+	Supplier supplier = new Supplier(id, name, contactEmail);
+	Supplier savedSupplier = this.supplierRepository.save(supplier);
+	return savedSupplier;
+    }
+
+    public void removeSupplier(int supplierId) {
+	this.supplierRepository.delete(supplierId);
+    }
+
+    public Supplier updateSupplier(int id, String newName, String newContactEmail) {
+	Supplier supplierToUpdate = new Supplier(id, newName, newContactEmail);
+	Supplier updateSupplier = this.supplierRepository.update(supplierToUpdate);
+	return updateSupplier;
+    }
+
+    public Iterable<Supplier> findAll() {
+	return this.supplierRepository.findAll();
+    }
+
+    public Supplier findById(int id) {
+	return this.supplierRepository.findById(id);
+    }
 }
